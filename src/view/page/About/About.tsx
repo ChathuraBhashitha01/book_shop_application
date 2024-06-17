b5d7f02b17aa4248bbf8769b3e9604cd7d41f4d5
MSI <chathurabhashitha01@gmail.com>
2024-06-17 12:23:36 +0530

    about form creation

--- src/view/page/About/About.tsx
+++ src/view/page/About/About.tsx
@@ -5,7 +5,7 @@ export class About extends Component {
     render() {
         return (
             <div className="h-[88.3vh]">
-                <img src={icon} alt="" className="h-[25vh] w-[100vw] filter brightness-150 opacity-80"/>
+                <img src={icon} alt="" className="h-[30vh] w-[100vw] filter brightness-150 opacity-40"/>
                 <div className="w-[30vh] h-[30vh] bottom-1 border-black">
 
                 </div>
